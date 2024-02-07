25046268bd2895e0b7ba16864d7b627b2098ec84
Aleksandra <a.haniok@gmail.com>
2024-02-07 18:22:20 +0000

    Move emoji flag 'converter'  to a separate component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,8 @@
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import styles from './City.module.css';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { flagemojiToPNG } from './FlagEmoji';
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat('en', {
@@ -22,15 +23,6 @@ function City() {
     },
     [id]
   );
-
-  const flagemojiToPNG = (flag) => {
-    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-      .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-      .join('');
-    return (
-      <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
-    );
-  };
 
   const { cityName, emoji, date, notes } = currentCity;
 
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
+import { flagemojiToPNG } from './FlagEmoji';
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat('en', {
@@ -7,15 +8,6 @@ const formatDate = (date) =>
     month: 'long',
     year: 'numeric',
   }).format(new Date(date));
-
-const flagemojiToPNG = (flag) => {
-  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-    .join('');
-  return (
-    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
-  );
-};
 
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
