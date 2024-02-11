88b8498c2e8709e3d65759f16d494e471c8d23e6
Aleksandra <a.haniok@gmail.com>
2024-02-11 15:50:08 +0000

    Add functionality to create a New City, add a date picker

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,6 +7,10 @@ import { useUrlPosition } from '../hooks/useUrlPosition';
 import { flagemojiToPNG } from './FlagEmoji';
 import Message from './Message';
 import Spinner from './Spinner';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { useCities } from '../contexts/CitiesContext';
+import { useNavigate } from 'react-router-dom';
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -27,6 +31,8 @@ function Form() {
   const [notes, setNotes] = useState('');
   const [emoji, setEmoji] = useState('');
   const [geocodingError, setGeocodingError] = useState('');
+  const { createCity, isLoading } = useCities();
+  const navigate = useNavigate();
 
   useEffect(
     function () {
@@ -58,6 +64,26 @@ function Form() {
     [lat, lng]
   );
 
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: {
+        lat: Number(lat),
+        lng: Number(lng),
+      },
+    };
+    await createCity(newCity);
+    navigate('/app/cities');
+  }
+
   // display a spinner when fetching location data
   if (isLoadingGeocoding) return <Spinner />;
 
@@ -67,7 +93,10 @@ function Form() {
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ''}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
@@ -81,10 +110,12 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        <input
+
+        <DatePicker
           id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
         />
       </div>
 
