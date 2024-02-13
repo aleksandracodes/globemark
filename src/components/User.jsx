0e13ed891666667fcdcb5400cbf773a58a1d4908
Aleksandra <a.haniok@gmail.com>
2024-02-13 19:20:31 +0000

    Add Fake Authentication: Set up Context

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,10 @@
-import styles from "./User.module.css";
+import styles from './User.module.css';
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
-  password: "qwerty",
-  avatar: "https://i.pravatar.cc/100?u=zz",
+  name: 'Laura',
+  email: 'laura@example.com',
+  password: 'qwerty',
+  avatar: 'https://i.pravatar.cc/100?u=ac',
 };
 
 function User() {
